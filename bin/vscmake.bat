9105c12e33550dcf2027e496e7bd6d3d083afbcd
Simon Lübeß <simon.luebess@gmail.com>
2022-04-12 14:32:00 +0200

    Added buildcommand

---
+++ bin/vscmake.bat
@@ -0,0 +1,9 @@
+@if not defined _echo echo off
+
+for /f "usebackq tokens=*" %%i in (`vswhere.exe -latest -products * -requires Microsoft.VisualStudio.Component.VC.CMake.Project -property installationPath`) do (
+    if exist "%%i\VC\Auxiliary\Build\vcvarsall.bat" (
+        "%%i\VC\Auxiliary\Build\vcvarsall.bat" %1
+        cd /D %2
+        cmake --preset %3
+    )
+)
