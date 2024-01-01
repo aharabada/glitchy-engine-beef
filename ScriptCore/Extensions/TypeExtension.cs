c111fff32bd05ebd7fdd8eaee22fa96f15c417af
Simon Lübeß <simon.luebess@gmail.com>
2024-01-01 13:02:50 +0100

    ScriptCore: Added extension Type.IsAssignableTo

---
+++ ScriptCore/Extensions/TypeExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GlitchyEngine.Extensions;
+
+public static class TypeExtension
+{
+    /// <summary>
+    /// Determines whether the current type can be assigned to a variable of the specified <see cref="targetType"/>.
+    /// </summary>
+    /// <param name="targetType">The type to compare with the current type.</param>
+    /// <returns></returns>
+    public static bool IsAssignableTo(this Type type, Type targetType)
+    {
+        return targetType.IsAssignableFrom(type);
+    }
+}
